3d9e1889e4dfccc350265c23d89be091e3865056
Thuan Ngo Duc <thuan@Thuans-MacBook-Pro.local>
2020-10-02 09:32:11 +0900

    Add source code for ch03/ex12

---
+++ ch03/ex12/ex12.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"sort"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Not enough parameters. Add 2 strings to the parameters!")
+	}
+	firstText := os.Args[1]
+	secondText := os.Args[2]
+	fmt.Printf("Is Anagram? %v\n", anagram(firstText, secondText))
+}
+
+func anagram(s1, s2 string) bool {
+	return sortStr(s1) == sortStr(s2)
+}
+
+func sortStr(k string) string {
+	s := strings.Split(k, "")
+	sort.Strings(s)
+
+	return strings.Join(s, "")
+}
