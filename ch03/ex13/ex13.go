f053d1d7408142388ae2011eabd4273faf38c062
Thuan Ngo Duc <thuan@Thuans-MacBook-Pro.local>
2020-10-02 10:00:31 +0900

    Add source code for ch03/ex13

---
+++ ch03/ex13/ex13.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+const (
+	KB, MB, GB, TB, PB, EB, ZB, YB = 1e3, 1e6, 1e9, 1e12, 1e15, 1e18, 1e21, 1e24
+)
+
+func main() {
+	fmt.Println(KB, ",", MB, ",", GB, ",", TB, ",", PB, ",", EB, ",", ZB, ",", YB)
+}
