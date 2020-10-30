d3b50e26645fde42c16f77b162424512f4c26618
Thuan Ngo Duc <thuan@Thuans-MacBook-Pro.local>
2020-10-30 11:40:46 +0900

    Add ch05/ex10

---
+++ ch05/ex10/ex10.go
@@ -0,0 +1,66 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 136.
+
+// The toposort program prints the nodes of a DAG in topological order.
+package main
+
+import (
+	"fmt"
+)
+
+//!+table
+// prereqs maps computer science courses to their prerequisites.
+var prereqs = map[string]map[string]bool{
+	"algorithms": {"data structures": true},
+	"calculus":   {"linear algebra": true},
+	"compilers": {
+		"data structures":       true,
+		"formal languages":      true,
+		"computer organization": true,
+	},
+
+	"data structures":      {"discrete math": true},
+	"databases":            {"data structures": true},
+	"discrete math":        {"intro to programming": true},
+	"formal languages":     {"discrete math": true},
+	"networks":             {"operating system": true},
+	"operating system":     {"data structures": true, "computer organization": true},
+	"programming language": {"data structures": true, "computer organization": true},
+}
+
+//!-table
+
+//!+main
+func main() {
+	for i, course := range topoSort(prereqs) {
+		fmt.Printf("%d:\t%s\n", i+1, course)
+	}
+}
+
+func topoSort(m map[string]map[string]bool) []string {
+	var order []string
+	seen := make(map[string]bool)
+	var visitAll func(items map[string]bool)
+
+	visitAll = func(items map[string]bool) {
+		for item := range items {
+			fmt.Println(item)
+			if !seen[item] {
+				seen[item] = true
+				visitAll(m[item])
+				order = append(order, item)
+			}
+		}
+	}
+
+	keys := make(map[string]bool)
+	for key := range m {
+		keys[key] = true
+	}
+	visitAll(keys)
+	return order
+}
+
+//!-main
